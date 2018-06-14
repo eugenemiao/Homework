92c550afd12391ca49de32d84900a5ac77dbbfef
eugenemiao <36180255+eugenemiao@users.noreply.github.com>
2018-06-13 21:51:09 -0700

    Delete config.py

--- Week 7 - Social Analytics/config.py
+++
@@ -1,8 +0,0 @@
-# These keys are not valid. Use your own API keys
-
-consumer_key = "Gu6vV0GLxZFowUaVu5vmHSI2b"
-consumer_secret = "C2LzJudI5xc3Qh44UVVOgJXybxAKIhcYWbmCjGAMcM7N5SkXNR"
-access_token = "1002964688810831872-A9jMAoLF5Dfph5iRGSLodj1ZK26V09"
-access_token_secret = "WT7q5loAQ3sui45VZyBj3kAr2rm2vnNhBvPIdDcWgeawO"
-
-weather_api_key = "25bc90a1196e6f153eece0bc0b0fc9eb"
